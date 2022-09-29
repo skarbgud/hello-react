9f7376da022012fe435d71b08e11604af2becff0
GyuHyeong Nam <sjarbgud@gmail.com>
2022-09-29 23:17:45 +0900

    3.3 props

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,26 @@
-const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+import PropsTypes from 'prop-types';
+
+const MyComponent = ({ name, favoriteNumber, children }) => {
+    return (
+        <div>
+            안녕하세요, 제 이름은 {name}  입니다. <br />
+            children 값은 {children}
+            입니다.
+            <br />
+            제가 좋아하는 숫자는 {favoriteNumber}입니다.
+        </div>
+    );
+};
+
+// props 기본 설정
+MyComponent.defaultProps = {
+    name: '기본 이름'
+};
+
+//name의 값이 문자열(string) 형태로 전달해야한다
+MyComponent.propTypes = {
+    name: PropsTypes.string,
+    favoriteNumber: PropsTypes.number.isRequired
 };
 
 export default MyComponent;
