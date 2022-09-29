3b51cf6c9b5e1348792d2b7519f288261d7df6cb
GyuHyeong Nam <sjarbgud@gmail.com>
2022-09-29 22:58:46 +0900

    3.1 클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
 import './App.css';
-import { Fragment } from 'react';
+import { Component } from 'react';
 
-function App() {
-  const name = '리액트';
-  const name2 = '뤼액트';
-  const name3 = undefined;
-  const style = {
-    // background-color는 backgroundColor와 같이 - 가 사라지고 카멜 표기법으로 작성됩니다.
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16
+class App extends Component {
+  render() {
+    const name = '리액트';
+    return <div className="react">{name}</div>;
   }
-
-  return (
-    // <Fragment> => <> </>로 바꿀 수 있다.
-    <>
-      {/* 2.4.3 */}
-      {name === '리액트' ? (
-        <h1>{name} 입니다.0</h1>
-      ) : (
-        <h2>{name}가 아닙니다.</h2>
-      )}
-      {/* 2.4.4 */}
-      <div>{name2 === '리액트' ? <h1>리액트입니다.1</h1> : null}</div>
-      <div>{name2 === '리액트' && <h1>리액트입니다.2</h1>}</div>
-      {/* 2.4.5 */}
-      <div>{name3 || '리액트'}</div>
-      {/* 2.4.6 */}
-      <div style={style}>{name}</div>
-      {/* 2.4.7 */}
-      <div className='react'>{name}</div>
-      {/* 2.4.8 */}
-      <input/>
-    </>
-    // </Fragment>
-  );
 }
 
 export default App;
