10d52f7a331b9854c557f1f83c3c0b908ed3fbf9
GyuHyeong Nam <sjarbgud@gmail.com>
2022-10-02 05:05:16 +0900

    5. ref: DOM에 이름 달기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 // import Counter from "./Counter";
 // import Say from "./Say";
-import EventPractice from "./EventPractice";
+// import EventPractice from "./EventPractice";
+// import ValidationSample from "./ValidationSample";
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
+class App extends Component {
   // return <Counter />;
   // return <Say />;
-  return <EventPractice />
+  // return <EventPractice />
+  // return <ValidationSample />
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 };
 
 export default App;
