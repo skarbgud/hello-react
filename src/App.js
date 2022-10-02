27edce93546fcb2b412ad1bc797a975e01ea897d
GyuHyeong Nam <sjarbgud@gmail.com>
2022-10-03 02:03:25 +0900

    7. 컴포넌트의 라이프사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-// import Counter from "./Counter";
-// import Say from "./Say";
-// import EventPractice from "./EventPractice";
-// import ValidationSample from "./ValidationSample";
 import { Component } from "react";
-// import ScrollBox from "./ScrollBox";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+// 랜덤 색상을 생성합니다.
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
-  // return <Counter />;
-  // return <Say />;
-  // return <EventPractice />
-  // return <ValidationSample />
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  state = {
+    error: false,
+  };
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true,
+    });
+  }
+  render() {
+    if (this.state.error) return <div>에러가 발생했습니다!</div>;
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
