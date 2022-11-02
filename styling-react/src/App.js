82580a61807e9c2a6a8a3e3bcf6572afe387de09
GyuHyeong Nam <sjarbgud@gmail.com>
2022-11-02 21:48:42 +0900

    9.2 Sass 사용하기

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,25 +1,11 @@
 import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SassComponent from './SassComponent';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <img src={logo} className="logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SassComponent />
       </div>
     )
   }
