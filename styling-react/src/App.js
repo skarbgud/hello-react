fc97ee3afbcb2a3e2ba030014c665041493079a1
GyuHyeong Nam <sjarbgud@gmail.com>
2022-11-11 23:45:26 +0900

    9.4 styled-components

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,11 @@
 import { Component } from 'react';
-import CSSModule from './CSSModule';
-import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSModule/>
+        <StyledComponent/>
       </div>
     )
   }
