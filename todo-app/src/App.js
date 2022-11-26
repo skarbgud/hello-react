f01894c10d30da31a87a7b2b4e27c79b3024d91d
GyuHyeong Nam <sjarbgud@gmail.com>
2022-11-26 17:46:07 +0900

    11.5.2 useReducer 사용하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,58 +2,66 @@ import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useReducer, useRef } from 'react';
+
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': //새로 추가
+      return todos.concat(action.todo);
+    case 'REMOVE': //제거
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE': //토글
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
 
 const App = () => {
-  function createBulkTodos() {
-    const array = [];
-    for (let i = 1; i <= 2500; i++) {
-      array.push({
-        id: i,
-        text: `할일 ${i}`,
-        checked: false,
-      });
-    }
-    return array;
-  }
-
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos => todos.concat(todo));
-      nextId.current += 1; // nextId 1씩 더하기
-    },[]);
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1; // nextId 1씩 더하기
+  }, []);
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
-    },[]);
+  const onRemove = useCallback(id => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(todos =>
-        todos.map(todo => 
-          todo.id === id ? {...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },[]);
+  const onToggle = useCallback(id => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
-  )
+  );
 };
 
 export default App;
