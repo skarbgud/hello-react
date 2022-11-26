2d07580faa1096893fe1e1166b4a8f1cc55f52f8
GyuHyeong Nam <sjarbgud@gmail.com>
2022-11-26 21:08:22 +0900

    11.8 react-virtualized를 사용한 렌더링 최적화

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,19 +1,37 @@
-import TodoListItem from "./TodoListItem";
+import React, { useCallback } from 'react';
 import './TodoList.scss';
+import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-    return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem
-                    todo={todo} 
-                    key={todo.id} 
-                    onRemove={onRemove} 
-                    onToggle={onToggle}
-                />
-            ))}
-        </div>
-    );
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer} //항목을 렌더링할 때 쓰는 함수
+      list={todos}
+      style={{ outline: 'none' }} //List에 기본 적용되는 outline 스타일 제거
+    />
+  );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
